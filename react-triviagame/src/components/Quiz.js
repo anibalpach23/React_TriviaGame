50bd9e5767fb52e28f267c5055c520dd1a62f252
Anibal Pacheco <pachecol.anibala@gmail.com>
2018-11-05 12:52:56 -0500

    finished code. gotta fix button event

--- react-triviagame/src/components/Quiz.js
+++ react-triviagame/src/components/Quiz.js
@@ -3,29 +3,40 @@ import PropTypes from "prop-types";
 import Question from "../components/Question";
 import QuestionCount from "../components/QuestionCount";
 import AnswerOption from "../components/AnswerOption";
+import { CSSTransitionGroup } from "react-transition-group";
 
 function Quiz(props) {
-  return (
-    <div className="quiz">
-      <QuestionCount counter={props.questionId} total={props.questionTotal} />
-      <Question content={props.question} />
-      <ul className="answerOptions">
-        {props.answerOptions.map(renderAnswerOptions)}
-      </ul>
-    </div>
-  );
-}
+  function renderAnswerOptions(key) {
+    return (
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={props.questionId}
+        questionId={props.questionId}
+        onAnswerSelected={props.onAnswerSelected}
+      />
+    );
+  }
 
-function renderAnswerOptions(key) {
   return (
-    <AnswerOption
-      key={key.content}
-      answerContent={key.content}
-      answerType={key.type}
-      answer={props.questionId}
-      questionId={props.questionId}
-      onAnswerSelected={props.onAnswerSelected}
-    />
+    <CSSTransitionGroup
+      className="container"
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}
+    >
+      <div key={props.questionId}>
+        <QuestionCount counter={props.questionId} total={props.questionTotal} />
+        <Question content={props.question} />
+        <ul className="answerOptions">
+          {props.answerOptions.map(renderAnswerOptions)}
+        </ul>
+      </div>
+    </CSSTransitionGroup>
   );
 }
 
